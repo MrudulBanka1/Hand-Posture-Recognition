ce21e1e34d05afccabbc5c0d708b85a7efb01c03
Mrudul Banka <bankamrudul@gmail.com>
2019-01-28 18:07:34 -0600

    Add files via upload

---
+++ trainClassifier.m
@@ -0,0 +1,54 @@
+function [trainedClassifier, validationAccuracy] = trainClassifier(trainingData)
+% returns a trained classifier and its accuracy. 
+% Extract predictors and response
+% This code processes the data into the right shape for training the
+% model.
+% Convert input to table
+inputTable = array2table(trainingData, 'VariableNames', {'column_1', 'column_2', 'column_3', 'column_4', 'column_5', 'column_6', 'column_7', 'column_8', 'column_9', 'column_10', 'column_11', 'column_12', 'column_13'});
+
+predictorNames = {'column_2', 'column_3', 'column_4', 'column_5', 'column_6', 'column_7', 'column_8', 'column_9', 'column_10', 'column_11', 'column_12', 'column_13'};
+predictors = inputTable(:, predictorNames);
+response = inputTable.column_1;
+isCategoricalPredictor = [false, false, false, false, false, false, false, false, false, false, false, false];
+
+% Train a classifier
+% This code specifies all the classifier options and trains the classifier.
+template = templateTree(...
+    'MaxNumSplits', 77404);
+classificationEnsemble = fitcensemble(...
+    predictors, ...
+    response, ...
+    'Method', 'Bag', ...
+    'NumLearningCycles', 30, ...
+    'Learners', template, ...
+    'ClassNames', [1; 2; 3; 4; 5]);
+
+% Create the result struct with predict function
+predictorExtractionFcn = @(x) array2table(x, 'VariableNames', predictorNames);
+ensemblePredictFcn = @(x) predict(classificationEnsemble, x);
+trainedClassifier.predictFcn = @(x) ensemblePredictFcn(predictorExtractionFcn(x));
+
+% Add additional fields to the result struct
+trainedClassifier.ClassificationEnsemble = classificationEnsemble;
+trainedClassifier.About = 'This struct is a trained model exported from Classification Learner R2018b.';
+trainedClassifier.HowToPredict = sprintf('To make predictions on a new predictor column matrix, X, use: \n  yfit = c.predictFcn(X) \nreplacing ''c'' with the name of the variable that is this struct, e.g. ''trainedModel''. \n \nX must contain exactly 12 columns because this model was trained using 12 predictors. \nX must contain only predictor columns in exactly the same order and format as your training \ndata. Do not include the response column or any columns you did not import into the app. \n \nFor more information, see <a href="matlab:helpview(fullfile(docroot, ''stats'', ''stats.map''), ''appclassification_exportmodeltoworkspace'')">How to predict using an exported model</a>.');
+
+% Extract predictors and response
+% This code processes the data into the right shape for training the
+% model.
+% Convert input to table
+inputTable = array2table(trainingData, 'VariableNames', {'column_1', 'column_2', 'column_3', 'column_4', 'column_5', 'column_6', 'column_7', 'column_8', 'column_9', 'column_10', 'column_11', 'column_12', 'column_13'});
+
+predictorNames = {'column_2', 'column_3', 'column_4', 'column_5', 'column_6', 'column_7', 'column_8', 'column_9', 'column_10', 'column_11', 'column_12', 'column_13'};
+predictors = inputTable(:, predictorNames);
+response = inputTable.column_1;
+isCategoricalPredictor = [false, false, false, false, false, false, false, false, false, false, false, false];
+
+% Perform cross-validation
+partitionedModel = crossval(trainedClassifier.ClassificationEnsemble, 'KFold', 5);
+
+% Compute validation predictions
+[validationPredictions, validationScores] = kfoldPredict(partitionedModel);
+
+% Compute validation accuracy
+validationAccuracy = 1 - kfoldLoss(partitionedModel, 'LossFun', 'ClassifError');
